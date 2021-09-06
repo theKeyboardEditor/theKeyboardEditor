a0fec63b89e77baf10e473c8dd4022e2990b58bf
logo4poop <logo4poop@protonmail.com>
2021-09-06 17:10:16 -0400

    Add github actions script, fix compilation error on native platforms (#4)
    * Added workflow for compilation
    
    * Fixed error on static platforms
    
    * Fix windows build command

--- src/MenuTabElement.hx
+++ src/MenuTabElement.hx
@@ -9,9 +9,9 @@ import kha.graphics2.Graphics;
 class MenuTabElement {
 	public var handle:Dynamic;
 
-	public function new(x: Int, y: Int, title: String, options: Map<String, Function>, ui: Zui) {
+	public function new(x: Int = 0, y: Int = 28, title: String, options: Map<String, Function>, ui: Zui) {
 		handle = Id.handle();
-		if (y == null) y = 28;
+		
 		if (ui.window(handle, x, y, Std.int(System.windowWidth()/10), System.windowHeight()-y)) {
 			for (option in options.keys()) {
 				if (ui.button(option, Left)) {
