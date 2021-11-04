97a738a9b3c240fe6e30931e7ad5d42684bf3385
Isaac Silverstein <logo4poop@protonmail.com>
2021-11-04 18:20:58 -0400

    Add Keyson as a dependency

--- khafile.js
+++ khafile.js
@@ -1,10 +1,16 @@
 let project = new Project('theKeyboardEditor');
+
+// Configration
 project.localLibraryPath = 'vendor';
 project.icon = 'assets/Icon.png';
 
+// Paths
 project.addAssets('assets/**');
 project.addShaders('shaders/**');
 project.addSources('src');
+
 // Libraries
 project.addLibrary('zui');
+await project.addProject('vendor/keyson');
+
 resolve(project);
