364cee85598498c144baf4dcad1c37da98c80d24
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2023-12-22 19:25:49 +0100

    Variablize KeyRenderer.hx
    In effect just replaced all constants

--- src/KeyRenderer.hx
+++ src/KeyRenderer.hx
@@ -1,17 +1,56 @@
 package;
 
+import ceramic.Color;
 import ceramic.Visual;
 import haxe.ui.backend.ceramic.RoundedRect;
+
+var color1:Int;
+var color2:Int;
+var xInner:Float;
+var yInner:Float;
+var rInner:Float;
+var borderInner:Int;
+var thicknessInner:Int;
+var offsetInner:Float;
+var xOuter:Float;
+var yOuter:Float;
+var rOuter:Float;
+var borderOuter:Int;
+var thicknessOuter:Int;
+var scaleTo:Float;
+var sizeX:Int;
+var SizeY:Int;
 
 class KeyRenderer extends Visual {
 	override public function new() {
 		super();
-		size(54, 54);
-
-		final fg = new RoundedRect(0xffFCFCFC, 3, 1.5, 7, this.width - 12, this.height - 12);
+		
+		color1= 0xffFCFCFC;
+		color2= 0xffCCCCCC;
+		xInner= 3;
+		yInner= xInner/2;
+		rInner= 7;
+		borderInner= 0x00000000;
+		thicknessInner =1;
+		offsetInner=xInner*4;
+		xOuter= 0;
+		yOuter= 0;
+		rOuter= 7;
+		borderOuter= 0x00000000;
+		thicknessOuter= 1;
+		
+		scaleTo= 54/100;
+		sizeX=100;
+		SizeY=200;
+		
+		size(sizeX*scaleTo,SizeY*scaleTo);
+		
+		
+// public function new(color:Color, x:Float, y:Float, radius:Float, w:Float, h:Float, border:Color = Color.NONE, thickness:Int = 1)
+		final fg = new RoundedRect(color1, xInner, yInner, rInner, this.width - offsetInner, this.height - offsetInner, borderInner, thicknessInner);
 		this.add(fg);
 
-		final bg = new RoundedRect(0xffCCCCCC, 0, 0, 7, this.width, this.height);
+		final bg = new RoundedRect(color2, xOuter, yOuter, rOuter, this.width, this.height, borderOuter, thicknessOuter);
 		this.add(bg);
 	}
 }
