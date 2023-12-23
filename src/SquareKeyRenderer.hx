b79eff38f8991c52644bb25bb67dd583edcf15d5
fire-h0und <cest73@gmail.com>
2023-12-23 16:22:51 +0100

    Adjusted outline and offset for the key layers in SquareKeyRenderer

--- src/SquareKeyRenderer.hx
+++ src/SquareKeyRenderer.hx
@@ -7,13 +7,13 @@ import haxe.ui.backend.ceramic.RoundedRect;
 class SquareKeyRenderer extends Visual {
 	public var topColor: Int = 0xffFCFCFC;
 	public var bodyColor: Int = 0xFFCCCCCC;
-	public var xInner: Float = 3;
-	public var yInner: Float = 3 / 2;
+	public var xInner: Float = 4;
+	public var yInner: Float = 4 / 2;
 	public var borderInner: Int = 0x00000000;
-	public var thicknessInner: Int = 1;
-	public var offsetInner: Float = 3 * 4;
+	public var thicknessInner: Int = 0;
+	public var offsetInner: Float = 4 * 4;
 	public var borderOuter: Int = 0xFFAAAAAA;
-	public var thicknessOuter: Int = 1;
+	public var thicknessOuter: Int = 0;
 	public var scaleTo: Float = 54 / 100;
 	public var roundedCorner: Int = 12;
 
