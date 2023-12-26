f2e65cf8dcc4d4918dac8743c0956fda772d3ea6
fire-h0und <cest73@gmail.com>
2023-12-26 22:23:42 +0100

    Rewrote the shape string parsing code

--- src/Viewport.hx
+++ src/Viewport.hx
@@ -19,6 +19,13 @@ class Viewport extends Scene {
 	// Used for zooming
 	var originX: Float = 510;
 	var originY: Float = 60;
+	var keySize: Float = 0; // size of a key
+	var widthNorth: Int;
+	var heightNorth: Int;
+	var widthSouth: Int;
+	var heightSouth: Int;
+	var offsetSouthX: Int;
+	var offsetSouthY: Int;
 
 	override public function new(keyboard: Keyboard) {
 		super();
@@ -53,133 +60,85 @@ class Viewport extends Scene {
 		for (k in this.keyboard.keys) {
 			var key: Dynamic;
 			var keyLabel: LabelRenderer;
+			keySize=1; //reset to default
 
 			// TODO: Create some form of syntax that can define this information without this switch case creature
-			// if nothing is given it defaults to 1U
-			switch k.shape {
-				case "0.75U":
-					final width = unit * 0.75 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "1.25U":
-					final width = unit * 1.25 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "1.5U":
-					final width = unit * 1.5 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "1.75U":
-					final width = unit * 1.75 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "2U":
-					final width = unit * 2 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "2.25U":
-					final width = unit * 2.25 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "2.5U":
-					final width = unit * 2.5 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "2.75U":
-					final width = unit * 2.75 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "3U":
-					final width = unit * 3 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "3U Vertical":
-					final width = unit - gapX;
-					final height = unit * 3 - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "6.25U":
-					final width = unit * 6.25 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "6.5U":
-					final width = unit * 6.5 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "7U":
-					final width = unit * 7 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "9U":
-					final width = unit * 9 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "10U":
-					final width = unit * 10 - gapX;
-					final height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "2U Vertical":
-					final width = unit - gapX;
-					final height = unit * 2 - gapY;
-					key = new keys.RectangularKey(width, height);
-				case "ISO":
-					// Normal ISO
-					final widthNorth = 150 - gapX;
-					final heightNorth = 100 - gapY;
-					final widthSouth = 125 - gapX;
-					final heightSouth = 200 - gapY;
-					final offsetSouthX = 25;
-					final offsetSouthY = 0;
+			for (t in ["BAE","ISO","XT_2U","AEK"] ) { // the special shape cases
+				if ( k.shape.split(' ').indexOf(t) != -1 ) { // if shape found found go here
+					//trace ("in:",k.shape,"found:", t);
+					switch k.shape {
+						case "ISO":
+							// Normal ISO
+							widthNorth = 150 - gapX;
+							heightNorth = 100 - gapY;
+							widthSouth = 125 - gapX;
+							heightSouth = 200 - gapY;
+							offsetSouthX = 25;
+							offsetSouthY = 0;
+						case "ISO Inverted":
+							// Inverted ISO
+							// This is an ISO enter but with the top of the keycap reversed
+							widthNorth = 125 - gapX;
+							heightNorth = 200 - gapY;
+							widthSouth = 150 - gapX;
+							heightSouth = 100 - gapY;
+							offsetSouthX = 0;
+							offsetSouthY = 101; // the 101 is a code-desing quirk!
+						case "BAE":
+							// Normal BAE
+							widthNorth = 150 - gapX;
+							heightNorth = 200 - gapY;
+							widthSouth = 225 - gapX;
+							heightSouth = 100 - gapY;
+							offsetSouthX = -75;
+							offsetSouthY = 100;
+						case "BAE Inverted":
+							// Inverted BAE
+							widthNorth = 225 - gapX;
+							heightNorth = 100 - gapY;
+							widthSouth = 150 - gapX;
+							heightSouth = 200 - gapY;
+							offsetSouthX = -1; // the -1 is a code-design quirk!
+							offsetSouthY = 0;
+						case "XT_2U":
+							widthNorth = 100 - gapX;
+							heightNorth = 200 - gapY;
+							widthSouth = 200 - gapX;
+							heightSouth = 100 - gapY;
+							offsetSouthX = -100;
+							offsetSouthY = 100;
+						case "AEK":
+							widthNorth = 125 - gapX;
+							heightNorth = 100 - gapY;
+							widthSouth = 100 - gapX;
+							heightSouth = 200 - gapY;
+							offsetSouthX = 25;
+							offsetSouthY = 0;
+					} // now that the appropriate values are assigned, we apply them:
+					trace(widthNorth, heightNorth, widthSouth, heightSouth, offsetSouthX, offsetSouthY);
 					key = new keys.LShapeKey(widthNorth, heightNorth, widthSouth, heightSouth, offsetSouthX, offsetSouthY);
-				case "ISO Inverted":
-					// Inverted ISO
-					// This is an ISO enter but with the top of the keycap reversed
-					final widthNorth = 125 - gapX;
-					final heightNorth = 200 - gapY;
-					final widthSouth = 150 - gapX;
-					final heightSouth = 100 - gapY;
-					final offsetSouthX = 0;
-					final offsetSouthY = 101; // the 101 is a desing quirk!
-					key = new keys.LShapeKey(widthNorth, heightNorth, widthSouth, heightSouth, offsetSouthX, offsetSouthY);
-				case "BAE":
-					// Normal BAE
-					final widthNorth = 150 - gapX;
-					final heightNorth = 200 - gapY;
-					final widthSouth = 225 - gapX;
-					final heightSouth = 100 - gapY;
-					final offsetSouthX = -75;
-					final offsetSouthY = 100;
-					key = new keys.LShapeKey(widthNorth, heightNorth, widthSouth, heightSouth, offsetSouthX, offsetSouthY);
-				case "BAE Inverted":
-					// Inverted BAE
-					final widthNorth = 225 - gapX;
-					final heightNorth = 100 - gapY;
-					final widthSouth = 150 - gapX;
-					final heightSouth = 200 - gapY;
-					final offsetSouthX = -1; // the -1 is a design quirk!
-					final offsetSouthY = 0;
-					key = new keys.LShapeKey(widthNorth, heightNorth, widthSouth, heightSouth, offsetSouthX, offsetSouthY);
-				case "XT_2U":
-					final widthNorth = 100 - gapX;
-					final heightNorth = 200 - gapY;
-					final widthSouth = 200 - gapX;
-					final heightSouth = 100 - gapY;
-					final offsetSouthX = -100;
-					final offsetSouthY = 100;
-					key = new keys.LShapeKey(widthNorth, heightNorth, widthSouth, heightSouth, offsetSouthX, offsetSouthY);
-				case "AEK":
-					final widthNorth = 125 - gapX;
-					final heightNorth = 100 - gapY;
-					final widthSouth = 100 - gapX;
-					final heightSouth = 200 - gapY;
-					final offsetSouthX = 25;
-					final offsetSouthY = 0;
-					key = new keys.LShapeKey(widthNorth, heightNorth, widthSouth, heightSouth, offsetSouthX, offsetSouthY);
-				default:
-					// 1U
-					var width = unit - gapX;
-					var height = unit - gapY;
-					key = new keys.RectangularKey(width, height);
+					// why this does not get assigned right is beyond me
+				}
 			}
+			keySize = Std.parseFloat(k.shape); // every valid size will get caught here
+			if ( ! Math.isNaN(keySize) ) { 
+				if ( k.shape.split(' ').indexOf("Vertical") != -1 ) { // it's vertical!
+					width = unit - gapX;
+					height = unit * keySize - gapY;
+					trace ("|");
+				} else {
+					width = unit * keySize - gapX;
+					height = unit - gapY;
+					trace("-");
+				}
+				trace (width, height);
+				key = new keys.RectangularKey(width, height);
+			} else {
+				  trace ("fallthru",k.shape); // for some odd reason the special keys don't get assigned up there?
+				  trace(widthNorth, heightNorth, widthSouth, heightSouth, offsetSouthX, offsetSouthY);
+				  key = new keys.LShapeKey(widthNorth, heightNorth, widthSouth, heightSouth, offsetSouthX, offsetSouthY);
+			}
+			
 
 			key.pos(unit * k.position[Axis.X], unit * k.position[Axis.Y]);
 			key.onPointerDown(key, (_) -> {
@@ -200,7 +159,7 @@ class Viewport extends Scene {
 			// Key label rendering pass
 			var labelOffsetX: Float;
 			var labelOffsetY: Float;
-			for (l in k.labels) {
+			for (l in k.labels) {// we can have many labels!
 				keyLabel = new LabelRenderer(l.glyph);
 				keyLabel.labelColor = 0xFF00000F;
 				// is the label position set specifically?
