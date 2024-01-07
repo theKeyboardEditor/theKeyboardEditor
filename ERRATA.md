a2c6a0b30826051fb820dc04cb097296109d2a5a
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2024-01-07 17:17:23 +0100

    Added comments to KLE.hx and elsewhere (#223)
    * Added comments to KLE.hx and elsewhere
    
    * Added ISO/BAE import support to KLE

--- ERRATA.md
+++ ERRATA.md
@@ -1,4 +1,6 @@
 # Known bugs and workarounds
 
-None that we couldn't solve so far.
+0.1-preAlpha
+**Error**: When placing and deleting keys sometimes the undo might leave a key hanging.
+**Remedy**: The key can be selected and removed manually. The internal data structure seems not to be corrupted aside the presence of the superfluous key.
 
--- src/keyson/KLE.hx
+++ src/keyson/KLE.hx
@@ -13,34 +13,44 @@ class KLE {
 		var xNext: Float = 1;
 		var shape: String = "1U";
 
-		for (row in kle) {
-			row[0];
-			x = 0;
-			for (col in row) {
-				if (col.y != null)
-					y = y + col.y;
-				if (col.x != null)
-					xNext += col.x;
-				if (col is String) {
-					var legend: String = Std.string(col);
-					keyson.unit[0].addKey(shape, [x, y], legend);
-					w = 1;
-					shape = "1U";
-				} else {
-					w = if (col.w != null) col.w else if (col.h != null) col.h else 1;
-					shape = if (col.w != null) Std.string(w) + "U" else if (col.h != null) Std.string(w) + "U Vertical" else "1U";
-					x--;
+		// KLE's keyson has really odd fileds and relations?
+		for (row in kle) { // iterate thru rows
+			row[0]; // with calling this we force haxe alow iteration of row
+			x = 0; // reset vertical key position
+			for (col in row) { // iterate thru keys and gaps and all other stuff
+				if (col.y != null) // if we find a y: pair we add it as vertical row-to row padding
+					y = y + col.y; // effective immediately
+				if (col.x != null) // if we find a x: pair we assume it's key to key padding
+					xNext += col.x; // effective sfter placing the current element
+				if (col is String) { // we check if there is a "legend" in this columanr element 
+					var legend: String = Std.string(col); // yes, we take it in
+					trace("legend:",legend);
+					if      (shape == "ISO") // special case - for one reason or the other it renders 0.25U off to the right
+								keyson.unit[0].addKey(shape, [x-0.25, y], legend)
+					else
+								keyson.unit[0].addKey(shape, [x, y], legend);
+					w = 1; // default presumed width (if none is given)
+					shape = "1U"; // presumed shape
+				} else { // no there is no string in this element:
+					w = if (col.w != null) col.w else if (col.h != null) col.h else 1; // if we have w: pair we evaluate width
+					shape = if (col.w2 != null && col.w2 == 1.5) "ISO" 
+						 else if (col.w2 != null && col.w2 == 2.25) "BAE"
+						 else if (col.h != null) Std.string(w) + "U Vertical"
+						 else if (col.w != null) Std.string(w) + "U"
+								else "1U"; // w to shape
+					trace("shape:",shape);
+					x--; // we compensate default automatic horizontal stepping for this non key producing element
 				}
-				x = x + xNext;
+				x = x + xNext; // make horizontal stepping for the next item
 				if (col.w != null) {
-					xNext = col.w;
+					xNext = col.w; // accout for item position in the next iteration
 				} else {
 					xNext = 1;
 				}
 			}
-			y++;
+			y++; // make vertical stepping after the row is processed
 		}
-
+		// return what was converted
 		return keyson;
 	}
 
