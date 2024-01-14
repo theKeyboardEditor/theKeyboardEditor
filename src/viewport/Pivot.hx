7adacf7b1c3bb0bea72077eec8c85e089f19bbf4
fire-h0und <cest73@gmail.com>
2024-01-14 17:48:48 +0100

    We are ready to switch to supremely refined rounded corners now!

---
+++ src/viewport/Pivot.hx
@@ -0,0 +1,15 @@
+package viewport;
+
+import ceramic.Mesh;
+import ceramic.Arc;
+import ceramic.Color;
+import ceramic.Visual;
+
+/**
+ *
+ *    Draw a pivot marker on the given coordinates on the screen
+ * (a hollow circle with what would be a croos if it extended thru it's center)
+ *
+ */
+class Pivot extends Visual {
+}
