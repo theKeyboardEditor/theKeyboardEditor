ccc833e8e40f2ca138cdce053ef287ef9bac05ec
fire-h0und <cest73@gmail.com>
2024-02-05 18:49:14 +0100

    The HORROR should be fixed by this...

---
+++ src/actions/PlaceKey.hx
@@ -0,0 +1,54 @@
+package actions;
+
+import viewport.Viewport;
+import keyson.Keyson;
+import keyson.Axis;
+
+class PlaceKey extends Action {
+	final viewport: Viewport;
+	var placed: Array<KeyRenderer>;
+	final shape: String;
+	final unit: Int; // the receiving unit (numpad, left half...)
+	final x: Float; // in 1U keyson units
+	final y: Float;
+
+	override public function new(viewport: Viewport, unit: Int, shape: String, x: Float, y: Float) {
+		super();
+		this.viewport = viewport;
+		this.placed = placed.copy();
+		this.unit = unit;
+		this.x = x;
+		this.y = y;
+		this.shape = shape;
+	};
+
+	override public function act(type: ActionType) {
+		// if (type == Redo) {
+		// }
+		// TODO is there any difference with act and redo?
+		// Create a keyson key   second shape is legend!
+		final key = viewport.keyson.units[unit].addKey(shape, [x, y], shape);
+		// Draw that key
+		final keycap: KeyRenderer = KeyMaker.createKey(this.viewport.keyson.units[unit], key, this.viewport.unit, this.viewport.gapX,
+			this.viewport.gapY, keyboardUnit.keysColor)
+		keycap.pos(this.viewport.unit * key.position[Axis.X], this.viewport.unit * key.position[Axis.Y]);
+		this.viewport.workKeyboard.add(keycap);
+		this.placed = keycap;
+	}
+	super.act(type);
+}
+
+override public function undo() {
+	for (unit in viewport.keyson.units) {
+		for (key in unit.keys) {
+			if (placed.sourceKey == key) {
+				// clear keyson:
+				this.viewport.keyson.units[unit].removeKey(placed.sourceKey);
+				// clear Ceramic:
+				this.viewport.workKeyboard.dispose(placed);
+			}
+		}
+	}
+	super.undo();
+}
+}
