11ff2b7f3a17aee867b2236335a87dc5a2bb4da2
fire-h0und <cest73@gmail.com>
2024-02-05 18:24:19 +0100

    Actions yet to be broven working

--- src/actions/DeleteKeys.hx
+++ src/actions/DeleteKeys.hx
@@ -1,32 +1,42 @@
 package actions;
 
-import viewport.Viewport;
+mport viewport.Viewport;
+import keyson.Keyson;
+import keyson.Axis;
 
 class DeleteKeys extends Action {
 	final viewport: Viewport;
-	var deleted: Array<keyson.Keyson.Key> = [];
+	var deleted: Array<KeyRenderer>;
 
-	override public function new(viewport: Viewport) {
+	override public function new(viewport: Viewport, deleted: Array<KeyRenderer>) {
 		super();
 		this.viewport = viewport;
+		this.deleted = deleted.copy();
 	}
 
-	override public function act() {
-		for (key in viewport.selected.keys()) {
-			// Delete from viewport renderer
-			viewport.keyboard.keys.remove(key);
-			viewport.selected[key].destroy();
-			viewport.selected.remove(key);
+	override public function act(type: ActionType) {
+		// if (type == Redo) {
+		// }
+		// TODO is here any difference with act and redo?
 
-			this.deleted.push(key);
+		for (member in deleted) {
+			for (unit in viewport.keyson.units) {
+				// clear keyson:
+				this.viewport.keyson.units[unit].removeKey(member.sourceKey);
+				// clear Ceramic:
+				this.viewport.workKeyboard.dispose(member);
+			}
 		}
 		super.act();
 	}
 
 	override public function undo() {
-		for (key in deleted) {
-			this.viewport.keyboard.keys.push(key);
-			this.viewport.drawKey(key);
+		for (member in deleted) {
+			for (unit in viewport.keyson.units) {
+				// recreate keyson:
+				this.viewport.keyson.units[unit].addKey(member.sourceKey);
+				// recreate Ceramic:
+				this.viewport.workKeyboard.add(member);
+			}
 		}
 	}
-}
