28a65c232e7e37e5cf2d8ced323e3bc310a1885f
logo <logo4poop@protonmail.com>
2024-03-10 22:28:26 -0400

    Remove -> destroy

--- src/actions/DeleteKeys.hx
+++ src/actions/DeleteKeys.hx
@@ -2,35 +2,39 @@ package actions;
 
 import viewport.Viewport;
 import keyson.Keyson;
+import keyson.Axis;
 
 class DeleteKeys extends Action {
 	final viewport: Viewport;
 	final device: keyson.Keyboard; // the receiving unit
-	var deletees: Array<KeyRenderer>;
+	var deletedKeys: Array<KeyRenderer>;
 
-	override public function new(viewport: Viewport, device: keyson.Keyboard, deletees: Array<KeyRenderer>) {
+	override public function new(viewport: Viewport, device: keyson.Keyboard, deletedKeys: Array<KeyRenderer>) {
 		super();
 		this.viewport = viewport;
 		this.device = device;
-		this.deletees = deletees.copy();
+		this.deletedKeys = deletedKeys.copy();
 	}
 
 	override public function act(type: ActionType) {
-		for (member in deletees) {
+		for (member in deletedKeys) {
 			// clear keyson:
 			this.device.removeKey(member.sourceKey);
 			// clear Ceramic:
-			this.viewport.keycapSet.remove(member);
+			member.destroy();
 		}
 		super.act(type);
 	}
 
 	override public function undo() {
-		for (member in deletees) {
+		for (key in deletedKeys) {
+			final recreatedKey = KeyMaker.createKey(this.device, key.sourceKey, this.viewport.unit, this.viewport.gapX, this.viewport.gapY);
+			recreatedKey.pos(this.viewport.unit * key.sourceKey.position[Axis.X], viewport.unit * key.sourceKey.position[Axis.Y]);
+			recreatedKey.component('logic', new viewport.KeyLogic(viewport));
 			// recreate keyson:
-			this.device.insertKey(member.sourceKey);
+			this.device.insertKey(recreatedKey.sourceKey);
 			// recreate Ceramic:
-			this.viewport.keycapSet.add(member);
+			this.viewport.keycapSet.add(recreatedKey);
 		}
 	}
 }
--- src/actions/EditCut.hx
+++ src/actions/EditCut.hx
@@ -2,31 +2,32 @@ package actions;
 
 import viewport.Viewport;
 import keyson.Keyson;
+import keyson.Axis;
 
 class EditCut extends Action {
 	final viewport: Viewport;
 	final device: keyson.Keyboard; // the receiving unit
-	var cutees: Array<KeyRenderer>;
+	var cutKeys: Array<KeyRenderer>;
 
-	override public function new(viewport: Viewport, device: keyson.Keyboard, cutees: Array<KeyRenderer>) {
+	override public function new(viewport: Viewport, device: keyson.Keyboard, cutKeys: Array<KeyRenderer>) {
 		super();
 		this.viewport = viewport;
 		this.device = device;
-		this.cutees = cutees.copy();
+		this.cutKeys = cutKeys.copy();
 	}
 
 	override public function act(type: ActionType) {
 		final cloner = new cloner.Cloner();
 		// take in the selection to the editBuffer
 		CopyBuffer.selectedObjects.keys = [
-			for (shape in cutees) {
+			for (shape in cutKeys) {
 				// severe ties to the originals:
 				cloner.clone(shape.sourceKey);
 			}
 		];
 		CopyBuffer.selectedObjects.sortKeys();
 		// remove the selection from the keycapSet:
-		for (member in cutees) {
+		for (member in cutKeys) {
 			// clear keyson:
 			this.device.removeKey(member.sourceKey);
 			// clear Ceramic:
@@ -36,12 +37,15 @@ class EditCut extends Action {
 	}
 
 	override public function undo() {
-		// restore the cutees to the work surface
-		for (member in cutees) {
+		// restore the cutKeys to the work surface
+		for (key in cutKeys) {
+			final recreatedKey = KeyMaker.createKey(this.device, key.sourceKey, this.viewport.unit, this.viewport.gapX, this.viewport.gapY);
+			recreatedKey.pos(this.viewport.unit * key.sourceKey.position[Axis.X], viewport.unit * key.sourceKey.position[Axis.Y]);
+			recreatedKey.component('logic', new viewport.KeyLogic(viewport));
 			// recreate keyson:
-			this.device.insertKey(member.sourceKey);
+			this.device.insertKey(recreatedKey.sourceKey);
 			// recreate Ceramic:
-			this.viewport.keycapSet.add(member);
+			this.viewport.keycapSet.add(recreatedKey);
 		}
 		super.undo();
 	}
